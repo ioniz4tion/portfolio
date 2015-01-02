91dbafb28ee9ec64af5b04aa1565b0afa1710a91
Parker Piedmont <iqover9000@gmail.com>
2015-01-01 16:22:07 -0800

    initial commit

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	// $('.enter').fadeOut(0);
+
+	var $header = $('header');
+
+	var headerSize = function() {
+		var $windowHeight = $(window).height();
+		$header.css({ 'height' : $windowHeight + 'px' });
+	};
+
+	$(window).resize(function() {
+		headerSize();
+	});
+
+	$('.enter').click(function() {
+		$('.loader').removeClass('loader-open').addClass('loader-close');
+	});
+
+	$('.nav__button').click(function() {
+		$(this).toggleClass('open');
+		$('header').toggleClass('active');
+	})
+
+	// $('.loader').delay(24500).addClass('loader-close');
+
+	setTimeout(function() {
+	  $('.loader').fadeOut().empty();
+	}, 24500);
+
+	headerSize();
+
+});
+
+$(window).load(function() {
+	// $('.enter').delay(10000).addClass('enter-show');
+});
